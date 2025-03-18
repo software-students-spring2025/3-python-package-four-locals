2498452f195a4ca510e2d57e6f0d214b1c65737f
bdeweesevans <bdeweesevans@gmail.com>
2025-03-17 22:04:41 -0400

    modifying tests to work with remote pipeline
    lets see if this works

---
+++ devdice/__init__.py
@@ -0,0 +1,6 @@
+from .devdice import Chooser
+from .devdice import Eliminator
+from .devdice import MagicEightBall
+from .devdice import roulette
+from .devdice import flipACoin
+from .devdice import rollADice

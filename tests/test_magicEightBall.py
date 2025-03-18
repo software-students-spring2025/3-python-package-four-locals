a0506a60a372602df797ed05aaea57bf0ba376ba
Edwin <ec4085@nyu.edu>
2025-03-17 23:17:18 -0400

    Completed Tests

--- tests/test_magicEightBall.py
+++ tests/test_magicEightBall.py
@@ -6,28 +6,3 @@ def test_shake():
     ball = MagicEightBall()
 
     assert ball.shake() in ball.get_responses()
-
-
-def test_add_response():
-
-    ball = MagicEightBall()
-
-    ball.add_response("Test")
-    assert "Test" in ball.get_responses()
-
-
-def test_remove_response():
-
-    ball = MagicEightBall()
-
-    response = ball.get_responses()[0]
-    ball.remove_response(response)
-    assert response not in ball.get_responses()
-
-
-def test_remove_invalid_response():
-
-    ball = MagicEightBall()
-
-    with pytest.raises(ValueError):
-        ball.remove_response("Nonexistent response")

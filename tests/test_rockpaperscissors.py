3ee5551a5f19ac0b179d70aceed500d610168464
Zeklin <forrestwilliams@pureagave.org>
2025-03-19 12:16:01 -0400

    Updated RPS function and testing

--- tests/test_rockpaperscissors.py
+++ tests/test_rockpaperscissors.py
@@ -2,5 +2,19 @@ import pytest
 from devdice import rockPaperScissors
 
 def test_rockPaperScissors_valid():
+    outcomes = rockPaperScissors(5)
+    assert len(outcomes) == 5
+    for outcome in outcomes:
+        assert outcome in ['Rock', 'Paper', 'Scissors']
+    
+def test_rockPaperScissors_no_args():
     outcome = rockPaperScissors()
     assert outcome in ['Rock', 'Paper', 'Scissors']
+
+def test_rockPaperScissors_zero():
+    with pytest.raises(ValueError):
+        rockPaperScissors(0)
+        
+def test_rockPaperScissors_negative():   
+    with pytest.raises(ValueError):
+        rockPaperScissors(-3)

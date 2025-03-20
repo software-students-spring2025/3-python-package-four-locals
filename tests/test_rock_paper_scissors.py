007ff9c9f9782a00210316391abd9236d60c8dc7
bdeweesevans <bdeweesevans@gmail.com>
2025-03-19 22:57:45 -0400

    fixed testcase issue

--- tests/test_rock_paper_scissors.py
+++ tests/test_rock_paper_scissors.py
@@ -1,11 +1,11 @@
 import pytest
-from devdice import rockPaperScissors
+from devdice import rock_paper_scissors
 
 def test_rock_paper_scissors_valid():
     '''
     Runs 5 r-p-s simulations and tests to make sure the outcomes are valid.
     '''
-    outcomes = rockPaperScissors(5)
+    outcomes = rock_paper_scissors(5)
     assert len(outcomes) == 5
     for outcome in outcomes:
         assert outcome in ['Rock', 'Paper', 'Scissors']
@@ -14,7 +14,7 @@ def test_rock_paper_scissors_no_args():
     '''
     Runs r-p-s with no arguments.
     '''
-    outcome = rockPaperScissors()
+    outcome = rock_paper_scissors()
     assert outcome in ['Rock', 'Paper', 'Scissors']
 
 def test_rock_paper_scissors_zero():
@@ -22,11 +22,11 @@ def test_rock_paper_scissors_zero():
     Runs r-p-s with argument 0.
     '''
     with pytest.raises(ValueError):
-        rockPaperScissors(0)
+        rock_paper_scissors(0)
 
 def test_rock_paper_scissors_negative():
     '''
     Runs r-p-s with a negative argument.
     '''
     with pytest.raises(ValueError):
-        rockPaperScissors(-3)
+        rock_paper_scissors(-3)
